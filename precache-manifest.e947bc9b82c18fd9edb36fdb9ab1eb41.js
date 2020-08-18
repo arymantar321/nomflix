7b2c9f934fcf9ef6896bf6de36b58f5d42a203a6
arymantar321 <arymantar321@gmail.com>
2020-08-18 19:05:33 +0900

    Updates

---
+++ precache-manifest.e947bc9b82c18fd9edb36fdb9ab1eb41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a08c25173e89d42751c10765430905",
+    "url": "/nomflix/index.html"
+  },
+  {
+    "revision": "f3e622cc99c51a9acc97",
+    "url": "/nomflix/static/js/2.00141197.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/nomflix/static/js/2.00141197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c20a71771bfcb70e721",
+    "url": "/nomflix/static/js/main.c9cc260e.chunk.js"
+  },
+  {
+    "revision": "1b1395881ee10a379ee2",
+    "url": "/nomflix/static/js/runtime-main.ce1f5564.js"
+  }
+]);
